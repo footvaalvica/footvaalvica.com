3572476248de34b4e6a0831c5bb7e0bf3defa388
Mateus Pinho <mateusleitepinho@gmail.com>
2026-03-10 20:09:36 +0000

    starting over again

--- README.md
+++
@@ -1,2 +0,0 @@
-# footvaalvica.com
-a simple static website for my stuff!
--- devenv.nix
+++ devenv.nix
@@ -3,50 +3,29 @@
 {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
-  env.LANG = "en_US.UTF-8";
 
   # https://devenv.sh/packages/
-  packages = with pkgs; [
-    hunspell
-    hunspellDicts.en_US
-    hugo
-    nodePackages.markdownlint-cli
-  ] ++ lib.optional stdenv.isLinux xclip ++ lib.optional stdenv.isDarwin pngpaste;
+  packages = [ pkgs.git ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
 
   # https://devenv.sh/processes/
-  processes = {
-    hugo.exec = "hugo serve";
-  };
- 
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
   # https://devenv.sh/services/
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts = {
-    mkpost.exec = ''
-      POST=content/post/$(date +%Y-%m-%d)-$1.md
-      hugo new $POST
-      nvim $POST
-      git add $POST
-      git commit -m"feat: Add $POST"
-    '';
-    pasteimg.exec = ''
-      FILE=static/images/$(date +%Y-%m-%d)-$1.png
-      if [ -x "$(command -v pngpaste)" ]; then
-        pngpaste $FILE
-        echo "<img src=\"/images/$(basename $FILE)\" width=700/>" | pbcopy
-      else
-        xclip -selection clipboard -t image/png -o > $FILE
-        echo "<img src=\"/images/$(basename $FILE)\" width=700/>" | xclip -selection clipboard
-      fi
-      git add "$FILE"
-    '';
-  };
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
 
-  enterShell = "hugo";
+  # https://devenv.sh/basics/
+  enterShell = ''
+    hello         # Run scripts directly
+    git --version # Use packages
+  '';
 
   # https://devenv.sh/tasks/
   # tasks = {
@@ -60,8 +39,8 @@
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
 
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks = {
-  # };
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
   # See full reference at https://devenv.sh/reference/options/
 }
